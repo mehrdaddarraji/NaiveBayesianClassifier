d553c96010a21ab811cfb1c215ac5930d5de0f16
Jorge Garcia <jorgegarcia525@gmail.com>
2019-02-06 16:05:01 +0100

    make dataframe function and vectorizer

--- nb-classifier.py
+++ nb-classifier.py
@@ -1,45 +1,61 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-print("Hello Hubba Bubba Haibo\n")
-
 import pandas as pd 
+import re
 from pandas import DataFrame
 
-file = open("lab_train.txt", "r")
-lines = file.readlines()
-lines.pop(0)
-file.close()
-
-reviews = []
-sentiments = []
-
-for line in lines: 
+# function to create dataframes for text and sentiment
+def sentimentDataFram(filename):
     
-    index = 0
-    char = ''
+    # import train data
+    file = open(filename, "r")
+    lines = file.readlines()
+    lines.pop(0)
+    file.close()
     
-    while char != ',' and index < len(line):
+    # data structure for text and sentiment
+    reviews = []
+    sentiments = []
+    
+    # a DataFrame for the text and the corresponding sentiment
+    for line in lines: 
         
-        char = line[index]
-        index += 1
+        index = 0
+        char = ''
         
-    review = line[index + 1:-7]
-    reviews.append(review)
-
-    score = int(line[-4])
-    
-    if score >= 3:
-        sentiments.append(1.0)
-        print("positive")
-    else:
-        sentiments.append(0.0)
-        print("neg")
+        # find the start of the comment
+        while char != ',' and index < len(line):
+            
+            char = line[index]
+            index += 1
         
-    
+        # get the text     
+        review = line[index + 1:-7]
+        reviews.append(review)
+        
+        # score of the the text turned into pos/neg
+        score = int(line[-4])
+        
+        if score >= 3:
+            sentiments.append(1.0)
+            print("positive")
+        else:
+            sentiments.append(0.0)
+            print("neg")
+            
+    # creating DataFrame out of text and sentiment
+    df = DataFrame({'Review':reviews, 'Sentiments':sentiments})
+    return df
 
-df = DataFrame({'Review':reviews, 'Sentiments':sentiments})
+# dataframes for train and test dataset
+train_df = sentimentDataFrame("lab_train.txt")
+test_df = sentimentDataFrame("lab_test.txt")
+
+# vectorizer for the strings
+re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
+def tokenize(s):
+    return re_tok.sub(r' \1 ', s).split()
+
+
+
+
+
+
